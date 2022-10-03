ebcccd46c0928430458635687848c16085a8a3df
MuriloM-Fernandes <murilo.mf06@gmail.com>
2022-10-03 19:54:07 -0300

    galeryFinalized

--- script/script.js
+++ script/script.js
@@ -19,3 +19,26 @@ window.addEventListener("load",function(){
     video.play();
     video.loop = true;
 });
+
+//carousel swiper
+var swiper = new Swiper(".carouselSwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//inicialização AOS fades
+AOS.init({
+    duration:2000,
+});
